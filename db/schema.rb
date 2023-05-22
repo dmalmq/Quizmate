80db019308a12d6fe06b427c9251ea66bd646b8c
Phuvit Jaruratkit <poohphuvit@hotmail.com>
2023-05-22 17:13:03 +0900

    No use

--- db/schema.rb
+++ db/schema.rb
@@ -25,5 +25,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_053001) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
