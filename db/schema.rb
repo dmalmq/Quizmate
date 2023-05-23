68068beed57b6b5e6919cc3da4472352b8420833
ameesha kumar <kumar.ameesha1@gmail.com>
2023-05-23 14:47:27 +0900

    reupdated the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_052408) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_054647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_052408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "score"
+    t.boolean "correct"
     t.index ["correct_option_id"], name: "index_questions_on_correct_option_id"
     t.index ["interest_id"], name: "index_questions_on_interest_id"
     t.index ["quiz_id"], name: "index_questions_on_quiz_id"
