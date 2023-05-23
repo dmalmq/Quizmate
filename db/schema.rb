d6075c0d21f889c5101094376a2d09740e0e5266
ameesha kumar <kumar.ameesha1@gmail.com>
2023-05-23 13:11:09 +0900

    updated the seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_23_030947) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_23_040832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_23_030947) do
     t.bigint "quiz_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score"
+    t.integer "total_correct"
     t.index ["correct_option_id"], name: "index_questions_on_correct_option_id"
     t.index ["interest_id"], name: "index_questions_on_interest_id"
     t.index ["quiz_id"], name: "index_questions_on_quiz_id"
