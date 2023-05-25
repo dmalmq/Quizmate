8c72a686beb4d89b8799ed6cf79603aaa34ee0ad
Markus Nowak <markusnowak99@gmail.com>
2023-05-25 17:51:44 +0900

    designed quiz show page

--- app/controllers/quizzes/questions_controller.rb
+++ app/controllers/quizzes/questions_controller.rb
@@ -1,7 +1,7 @@
 class Quizzes::QuestionsController < ApplicationController
 
   def show
-    @question = Question.find(params[:id])
+    @question = Question.includes([:interest]).find(params[:id])
     @quiz = Quiz.find(params[:quiz_id])
     @next_question = @quiz.questions.where(answered: false).first
   end
