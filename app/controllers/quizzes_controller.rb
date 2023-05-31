e7500f37102a171bb29f321e7f1d79e4854ef729
Phuvit Jaruratkit <poohphuvit@hotmail.com>
2023-05-31 17:22:15 +0900

    DOne

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -8,10 +8,10 @@ class QuizzesController < ApplicationController
     @answered = 0
     @title = @interests.pluck(:name)
     @user = current_user
-
     @result = @interests.map do |interest|
       interest.corrected_percentage
     end
+    stats(@interests)
   end
 
   def show
@@ -39,9 +39,31 @@ class QuizzesController < ApplicationController
     @quiz.corrected_times = 0
     @quiz.user = current_user
     @quiz.save
-    # question_per_interests = (8.to_f / Interest.all.count).ceil
-    @questions = []
+    question_create
+    redirect_to quiz_challenge_path(@quiz, @quiz.challenges.first)
+  end
 
+  private
+
+  def quiz_params
+    params.fetch(:quiz, {}).permit(:number_of_question) # Number of quizzes each day
+  end
+
+  def stats(interest)
+    corrected_percentage = []
+    interest.each do |interest|
+      percentage = ((interest.challenges.where(corrected: true).count.to_f / interest.challenges.count) * 100).ceil
+      corrected_percentage << { interest_name: interest.name, percentage: percentage }
+    end
+    corrected_percentage.sort_by! { |interest| interest[:percentage] }.reverse!
+    @most_correct_interest = corrected_percentage.first[:interest_name]
+    @least_correct_interest = corrected_percentage.last[:interest_name]
+    @most_correct_percentage = corrected_percentage.first[:percentage]
+    @least_correct_percentage = corrected_percentage.last[:percentage]
+  end
+
+  def question_create
+    @questions = []
     8.times do
       interest_sample = Interest.all.sample
       all_questions = interest_sample.questions.order(streak: :asc)
@@ -57,19 +79,11 @@ class QuizzesController < ApplicationController
       sampled_question = all_questions.sample
       @questions << sampled_question
     end
-
     n = 0
     @questions.each do |question|
       n += 1
       challenge = Challenge.new(quiz: @quiz, question:, index_in_quiz: n)
       challenge.save
     end
-    redirect_to quiz_challenge_path(@quiz, @quiz.challenges.first)
-  end
-
-  private
-
-  def quiz_params
-    params.fetch(:quiz, {}).permit(:number_of_question) # Number of quizzes each day
   end
 end
