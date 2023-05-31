094577377485ae12a43b4b4a65b0510b33c46a5b
Markus Nowak <markusnowak99@gmail.com>
2023-05-31 12:13:54 +0900

    deleting and creating of questions in interests show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
+  # Admin
   require "sidekiq/web"
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
+  # Production
   root to: "pages#home"
   resources :users, only: [:show, :index]
-  resources :interests, only: [:index, :show, :create, :destroy]
+  resources :interests, only: [:index, :show, :create, :destroy] do
+    resources :questions, only: [:create]
+  end
   resources :quizzes, only: [:index, :show, :create, :new] do
     resources :challenges, only: [:create, :new, :show]
     resources :questions, only: [:show], controller: "quizzes/questions"
   end
-  resources :questions
+  resources :questions, only: [:destroy]
 
   resources :challenges, only: [:destroy, :update]
   authenticate :user, ->(user) { user.admin? } do
@@ -17,9 +25,6 @@ Rails.application.routes.draw do
   end
 
 
-  authenticate :user, ->(user) { user.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
