3fe2202aa0d5654116d154ccc02d369ccd2219d4
Phuvit Jaruratkit <poohphuvit@hotmail.com>
2023-06-01 18:21:40 +0900

    DONE

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -3,7 +3,9 @@ class InterestsController < ApplicationController
   def index
     @interests = policy_scope(Interest)
     @interest = Interest.new
+    @all_interest = Interest.all
     @quiz = Quiz.new
+    stats(@all_interest)
   end
 
   def show
@@ -36,5 +38,24 @@ class InterestsController < ApplicationController
 
   def interest_params
     params.require(:interest).permit(:user, :name, :priority)
+  end
+
+  def stats(all_interest)
+    corrected_percentage = []
+    if all_interest == []
+      return
+    else
+      all_interest.each do |interest|
+        if interest.challenges.count >= 1
+          percentage = ((interest.challenges.where(corrected: true).count.to_f / interest.challenges.count) * 100).ceil
+          corrected_percentage << { interest_name: interest.name, percentage: percentage }
+        end
+      end
+      corrected_percentage.sort_by! { |interest| interest[:percentage] }.reverse!
+      @most_correct_interest = corrected_percentage.first[:interest_name]
+      @least_correct_interest = corrected_percentage.last[:interest_name]
+      @most_correct_percentage = corrected_percentage.first[:percentage]
+      @least_correct_percentage = corrected_percentage.last[:percentage]
+    end
   end
 end
